e14dbe7ef8f177b64554d2d01c5a6af243ada001
mrajaonson <m.rajaonson@posteo.net>
2023-02-21 16:29:54 +0100

    Ajout User bean

---
+++ src/main/java/fr/javacnam/domotique/beans/User.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.javacnam.domotique.beans;
+
+/**
+ *
+ * @author rajaonson
+ */
+public class User {
+
+    private String login;
+    private String pass;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+}
